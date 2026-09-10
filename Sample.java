e9e94baf456603bb2563fb94ef7557df0b34214d
Pratham Tiwari <prathamtiwari0123@gmail.com>
2026-09-10 11:20:01 +0530

    Update Sample.java with new content

--- Sample.java
+++ Sample.java
@@ -1,6 +1,8 @@
 public class Sample {
-    String Agent = Cortex_agent;
     public static void main(String[] args) {
         System.out.println("Cortex Agent");
     }
+}
+class Agent {
+    String Agent = Cortex_agent;
 }
